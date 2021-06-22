16fc90d1cb5e3b49d11e6d28a83c8232063c1d89
teresabanks1025 <85649222+teresabanks@users.noreply.github.com>
2021-06-22 21:25:45 +0000

    assignment #1 part 2 complete

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,9 +8,10 @@ let candidateName =  "";
 let question = "";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = ""; 
-let questions;
-let correctAnswers;
-let candidateAnswers = "Sally Ride" ;
+let questions = 
+["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters?", "(5 + 3)/2 * 10 = ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?", "What is the minimum crew size for the ISS?"];
+let correctAnswers = ["Sally Ride" , "True", "40" , "Trajectory", "3"];
+let candidateAnswers = ["Sally Ride", "True", "40", "trajectory", "3"];
 
 
 function askForName() {
@@ -22,8 +23,11 @@ function askForName() {
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  let question = input.question("Who was the first American woman in space? ");
- 
+  //let question = input.question("Who was the first American woman in space? ");
+ for (let i = 0; i < questions.length; i++){
+   let info = input.question(questions[i]);
+   
+ }
   
   
 }
@@ -31,11 +35,23 @@ function askQuestion() {
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
- if (candidateAnswers === correctAnswer) {
-   console.log("Correct");
- } else {
-   console.log("Incorrect");
- }
+ let gradedQuiz = `Candidate Name: ${candidateName}
+ 1). ${questions[0]}
+    Your Answer: ${candidateAnswers[0]}
+    Correct Answer: ${correctAnswers[0]}
+  2). ${questions[1]}
+    Your Answer: ${candidateAnswers[1]}
+    Correct Answer: ${correctAnswers[1]}
+  3). ${questions[2]}
+    Your Answer: ${candidateAnswers[2]}
+    Correct Answer: ${correctAnswers[2]}
+  4). ${questions[3]}
+    Your Answer: ${candidateAnswers[3]}
+    Correct Answer: ${correctAnswers[3]}
+  5). ${questions[4]}
+    Your Answer: ${candidateAnswers[4]}
+    Correct Answer: ${correctAnswers[4]}`;
+ console.log(gradedQuiz);
   let grade;
   
 
